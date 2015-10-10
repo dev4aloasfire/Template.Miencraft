1eb6de543064a61055924aeafdf0badb303bf183
Aloasfire4dev <development.aloasfire@gmail.com>
2015-10-10 18:26:30 -0400

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php
+//if Email is set 
+  if (isset($_REQUEST['email']))  {
+  
+  //Email information
+  $admin_email = "you@domain.com";
+  $email = $_REQUEST['email'];
+  $subject = $_REQUEST['subject'];
+  $comment = $_REQUEST['comment'];
+  
+  //send email
+  mail($admin_email, "$subject", $comment, "From:" . $email);
+  
+  //Email response
+  echo "Thank you for contacting us!";
+  }
+  
+  else  {
+?>
+//Form
+ <form method="post">
+  Email: <input name="email" type="text" /><br />
+  Subject: <input name="subject" type="text" /><br />
+  Message:<br />
+  <textarea name="comment" rows="15" cols="40"></textarea><br />
+  <input type="submit" value="Submit" />
+  </form>
+  
+<?php
+  }
+?>
